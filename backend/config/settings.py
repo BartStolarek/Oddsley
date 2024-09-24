00517bf2cc583a23b28357de15e1209669bc750a
Bart-Desktop <Bartjstolarek@gmail.com>
2024-09-24 21:25:18 +1000

    added in swagger for api documentation

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
